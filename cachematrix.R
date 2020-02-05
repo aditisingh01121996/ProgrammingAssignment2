ef8146388b48a242eaef4318dc6c0ba58a465fd3
aditisingh01121996 <59146060+aditisingh01121996@users.noreply.github.com>
2020-02-06 04:46:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program creates a special matrix object that can cache its inverse
+makeCacheMatrix <- function( m = matrix() ) {
 
-## Write a short comment describing this function
+	## Initialize the inverse property
+    i <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+    ## Function for setting the matrix
+    set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+    }
 
+    ## Function for getting the matrix
+    get <- function() {
+    	## Return the matrix
+    	m
+    }
+
+    ## Function for seting the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+    ## Function for getting the inverse of the matrix
+    getInverse <- function() {
+        ## Return the inverse property
+        i
+    }
+
+    ## Return a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Compute the inverse of the special matrix returned by "makeCacheMatrix" above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the "cachesolve" should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+
+    ## Return the inverse if its already set
+    if( !is.null(m) ) {
+            message("getting cached data")
+            return(m)
+    }
+
+    ## Fetch the matrix from our object
+    data <- x$get()
+
+    ## Calculate the inverse using matrix multiplication
+    m <- solve(data) %*% data
+
+    ## Set the inverse to the object
+    x$setInverse(m)
+
+    ## Return the matrix
+    m
 }
